eff0c61e94c09c651d80bec5f51fc4d11e082321
Rodimiro Cerrato E <rodyce@gmail.com>
2021-10-02 00:38:04 -0600

    Integrate Multicall library with ethers
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- src/loot-glass.ts
+++ src/loot-glass.ts
@@ -1,1 +1,187 @@
-console.log('Hello World\!\!')
+import { providers } from "@0xsequence/multicall";
+import { ethers } from "ethers";
+
+require("dotenv").config();
+
+const LOOT_CONTRACT = "0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7";
+
+const CONTRACT_ABI = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getChest",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getFoot",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getHand",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getHead",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getNeck",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getRing",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getWaist",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getWeapon",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const LIMIT = 1000;
+
+const ALCHEMY_API_TOKEN = process.env.ALCHEMY_API_TOKEN;
+
+async function main() {
+  const ethersProvider = ethers.getDefaultProvider("mainnet", {
+    alchemy: ALCHEMY_API_TOKEN,
+  });
+  const multicallProvider = new providers.MulticallProvider(ethersProvider);
+  const loot = new ethers.Contract(
+    LOOT_CONTRACT,
+    CONTRACT_ABI,
+    multicallProvider
+  );
+
+  const functionNames = CONTRACT_ABI.map((x) => x.name);
+  const attributes = functionNames.map((fname) => loot[fname](66));
+
+  const attributeNames: string[] = await Promise.all(attributes);
+  for (const attrName of attributeNames) {
+    console.log(attrName);
+  }
+}
+
+main();
